9db230b3b48c7066d570824a5526d5f09d3b515b
omerrevach <hairviction@gmail.com>
2025-02-27 12:20:38 +0200

    created the cd pipelines

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Create the .github/workflows directory and files
+mkdir -p .github/workflows
+touch .github/workflows/cd-auth.yaml
+touch .github/workflows/cd-frontend.yaml
+touch .github/workflows/cd-frontend.yaml
+touch .github/workflows/cd-trade.yaml
